161e54810e8e26766d6dfc46637303dee535d31b
ligmaster <136215596+ligmaster3@users.noreply.github.com>
2024-05-25 10:38:38 -0500

    Update menu.js

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $(window).scroll(function () {
-      if ($(window).scrollTop() > 0.7 * $(window).height()) {
-          $("#navbar").addClass("sticky-nav"); // Add the class to change the color
-      } else {
-          $("#navbar").removeClass("sticky-nav"); // Remove the class to revert the color
-      }
-  });
-});
